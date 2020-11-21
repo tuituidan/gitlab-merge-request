ea23bab3aeb3b3de0f2c0c215a70b99599e97406
zhujunhan <136375002@qq.com>
2020-11-21 13:51:29 +0800

    初始化

---
+++ src/main/java/com/tuituidan/gmr/GitlabMergeRequestApplication.java
@@ -0,0 +1,13 @@
+package com.tuituidan.gmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabMergeRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlabMergeRequestApplication.class, args);
+    }
+
+}
